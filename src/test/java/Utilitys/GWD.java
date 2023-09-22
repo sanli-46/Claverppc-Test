a4118695dd44294dbe6e2464e88365439362b565
Alperen Şanlı <alperensanli.46@gmail.com>
2023-09-22 19:59:12 +0300

    ilk commit

---
+++ src/test/java/Utilitys/GWD.java
@@ -0,0 +1,38 @@
+package Utilitys;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class GWD {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+
+        try {
+            Thread.sleep(3000L);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+
+
+        }
+
+    }
+}
