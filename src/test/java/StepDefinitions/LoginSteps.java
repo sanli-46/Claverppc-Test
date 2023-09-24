c3e66fb1718ef2dcabd117559bd30816ab93f2f2
Enes <enes.buke12@gmail.com>
2023-09-24 22:51:15 +0300

    First Update

---
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -0,0 +1,43 @@
+package StepDefinitions;
+
+import Pages.Elements;
+import Utilitys.GWD;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en_scouse.An;
+import org.testng.Assert;
+
+public class LoginSteps {
+    Elements elm=new Elements();
+    @Given("Navigate to CleverEcommerce")
+    public void navigateToCleverEcommerce() {
+        GWD.getDriver().get("https://cleverppc.com/prestashop4/");
+    }
+    @When("Enter username as {string} and password as {string} for negative login")
+    public void enterUsernameAsAndPasswordAsForNegativeLogin(String name, String password) {
+        elm.myClick(elm.HmSignInButton);
+        elm.mySendKeys(elm.emailInput,name);
+        elm.mySendKeys(elm.passwInput,password);
+        elm.myClick(elm.signInButton);
+    }
+    @Then("User should login failed")
+    public void userShouldLoginFailed() {
+        elm.verifyContainsText(elm.loginErrorMsg,"error");
+    }
+
+    @And("Enter username and password pozitive login")
+    public void enterUsernameAndPasswordPozitiveLogin() {
+        elm.myClick(elm.HmSignInButton);
+        elm.mySendKeys(elm.emailInput,"test.enes12@gmail.com");
+        elm.mySendKeys(elm.passwInput,"e.b1357");
+        elm.myClick(elm.signInButton);
+    }
+    @And("User should login successfully")
+    public void userShouldLoginSuccessfully() {
+        Assert.assertTrue(GWD.getDriver().getCurrentUrl()
+                .contains("my-acc"));
+        elm.myClick(elm.homeIcon);
+    }
+}
